87db4708a7c18c6d34a4bf578fdef6c574ec2f53
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-03-01 09:35:25 +0700

    add technology filter tag button functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const skillBars = document.querySelectorAll('.fade-in');
-
+const technologyFilterTags = document.querySelectorAll('#technology-filter-tags-container ul li button');
+const technologyFilterAllTag = document.querySelector('#technology-all-tag');
 
 const skillBarsObserver = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -11,4 +12,29 @@ const skillBarsObserver = new IntersectionObserver((entries) => {
 
 skillBars.forEach(skillBar => {
     skillBarsObserver.observe(skillBar);
+});
+
+//add & remove active class for filter tags
+technologyFilterTags.forEach(technologyFilterTag => {
+    console.log(technologyFilterTag)
+    technologyFilterTag.onclick = function() {
+        if(technologyFilterAllTag.classList.contains('active')){
+            technologyFilterAllTag.classList.remove('active')
+            technologyFilterTag.classList.toggle('active')
+        }
+           
+        else if (!technologyFilterAllTag.classList.contains('active') && technologyFilterTag.innerHTML === technologyFilterAllTag.innerHTML) {
+            // console.log(technologyFilterTag, 'me')
+            console.log('working')
+            technologyFilterTags.forEach(tag => {
+                tag.classList.remove('active')
+            });
+
+            technologyFilterAllTag.classList.add('active');
+        }
+        
+        else if (!technologyFilterAllTag.classList.contains('active')) {
+            technologyFilterTag.classList.toggle('active')
+        }; 
+    };
 });
