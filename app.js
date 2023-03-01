9d00070e5e48c419e8f1bda13061968cc31e9af7
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-03-01 09:49:41 +0700

    add type filter tag button functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const skillBars = document.querySelectorAll('.fade-in');
 const technologyFilterTags = document.querySelectorAll('#technology-filter-tags-container ul li button');
 const technologyFilterAllTag = document.querySelector('#technology-all-tag');
+const typeFilterTags = document.querySelectorAll('#type-filter-tags-container ul li button');
+const typeFilterAllTag = document.querySelector('#type-all-tag');
 
 const skillBarsObserver = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -14,7 +16,7 @@ skillBars.forEach(skillBar => {
     skillBarsObserver.observe(skillBar);
 });
 
-//add & remove active class for filter tags
+//add & remove active class for technology filter tags
 technologyFilterTags.forEach(technologyFilterTag => {
     console.log(technologyFilterTag)
     technologyFilterTag.onclick = function() {
@@ -24,8 +26,6 @@ technologyFilterTags.forEach(technologyFilterTag => {
         }
            
         else if (!technologyFilterAllTag.classList.contains('active') && technologyFilterTag.innerHTML === technologyFilterAllTag.innerHTML) {
-            // console.log(technologyFilterTag, 'me')
-            console.log('working')
             technologyFilterTags.forEach(tag => {
                 tag.classList.remove('active')
             });
@@ -35,6 +35,29 @@ technologyFilterTags.forEach(technologyFilterTag => {
         
         else if (!technologyFilterAllTag.classList.contains('active')) {
             technologyFilterTag.classList.toggle('active')
+        }; 
+    };
+});
+
+//add & remove active class for type filter tags
+typeFilterTags.forEach(typeFilterTag => {
+    console.log(typeFilterTag)
+    typeFilterTag.onclick = function() {
+        if(typeFilterAllTag.classList.contains('active')){
+            typeFilterAllTag.classList.remove('active')
+            typeFilterTag.classList.toggle('active')
+        }
+           
+        else if (!typeFilterAllTag.classList.contains('active') && typeFilterTag.innerHTML === typeFilterAllTag.innerHTML) {
+            typeFilterTags.forEach(tag => {
+                tag.classList.remove('active')
+            });
+
+            typeFilterAllTag.classList.add('active');
+        }
+        
+        else if (!typeFilterAllTag.classList.contains('active')) {
+            typeFilterTag.classList.toggle('active')
         }; 
     };
 });
