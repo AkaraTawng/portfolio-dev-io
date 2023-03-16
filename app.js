0f73898c32fc32c3610393a263cbab769767aebe
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-03-17 06:59:56 +0700

    comment out technology filter buttons

--- app.js
+++ app.js
@@ -22,10 +22,6 @@ typeFilterButtons.forEach(button => {
     });
 });
 
-/** show all projects function
- * add click event listener to type filter all tag
- * on click, add active to all project items
- * */ 
 const showAllProjects = () => {
     projectItems.forEach(item => {
         item.style.display = 'flex';
