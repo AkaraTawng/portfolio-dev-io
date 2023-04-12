e1f07e7d5b45b70bf4abb3b8e5ef3dd1954e8071
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-04-12 07:32:54 +0700

    add carousel-container div. add basic carousel functionality

--- app.js
+++ app.js
@@ -502,7 +502,26 @@ list.forEach(item => {
 
 
 
+/* owl carousel */
 
+$(document).ready(function(){
+    $('.carousel-container').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
+            }
+        }
+    })
+})
 
 
 
