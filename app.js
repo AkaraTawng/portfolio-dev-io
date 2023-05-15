4cc814c1e2a7f3ed60493aa4d463449a6bed943e
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-15 09:26:03 +0700

    make api call

--- app.js
+++ app.js
@@ -622,4 +622,16 @@ function scroll() {
 
 
 
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/quotes?category',
+    headers: { 'X-Api-Key': 'ZfbIMrJw+PzBWtEP4PEODg==AgbCsJCapOOD8PMZ'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
 
